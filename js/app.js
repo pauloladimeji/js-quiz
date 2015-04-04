0981fe119defebe368c59e0282408534221eafb1
Paul Oladimeji <pauloladimeji@gmail.com>
2015-04-04 16:09:12 +0400

    Create questions array

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
-app.js
+/*
+
+Version 1
+
+*/
+
+var Quiz = (function(){
+	var allQuestions = [
+		{
+			question: "Who is the Prime Minister of the UK?",
+			choices: ["Tony Blair", "David Cameron", "Barack Obama"],
+			correctAnswer: 1
+		},
+		{
+			question: "Who is the President of the US?",
+			choices: ["Vladimir Putin", "Barack Obama", "George Bush"],
+			correctAnswer: 1
+		},
+		{
+			question: "Who is the Governor of Lagos State?",
+			choices: ["Tunde Fashola", "Rauf Aregbesola", "Lola Bello"],
+			correctAnswer: 0
+		},
+		{
+			question: "WWhere is the Suez Canal?",
+			choices: ["Cuba", "Australia", "Egypt"],
+			correctAnswer: 2
+		},
+		{
+			question: "WWhat is the chemical symbol for Iron?",
+			choices: ["Fe", "Ir", "Ca"],
+			correctAnswer: 1
+		}
+	];
+})();
