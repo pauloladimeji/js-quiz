e7e11b4c67112a9adf4a0d94c01a6612de36d9ac
Paul Oladimeji <pauloladimeji@gmail.com>
2015-04-09 17:16:35 +0400

    Displays first question on page load

--- js/app.js
+++ js/app.js
@@ -4,7 +4,49 @@ Version 1
 
 */
 
+//shorter getElementById();
+function _(elem) {
+	return document.getElementById(elem);
+}
+
+//Event Delegation Utility function
+var EventUtil = {
+
+	addHandler: function(element, type, handler) {
+		if(element.addEventListener) {
+			element.addEventListener(type, handler, false);
+		}
+		else if(element.attachEvent) {
+			element.attachEvent("on" + type, element, handler);
+		} else {
+			element["on" + type] = handler;
+		}
+	},
+
+	removeHandler: function(element, type, handler) {
+		if(element.removeEventListener) {
+			element.removeEventListener(type, handler, false);
+		}
+		else if(element.detachEvent) {
+			element.detachEvent("on" + type, element, handler);
+		} else {
+			element["on" + type] = null;
+		}
+	},
+
+	//handling IE events
+	getEvent: function(event) {
+		return event ? event : window.event; //window.event for IE events. also event = event || window.event;
+	},
+
+	getTarget: function(event) {
+		return event.target || event.srcElement; 
+	}
+};
+
+
 var Quiz = (function(){
+
 	var allQuestions = [
 		{
 			question: "Who is the Prime Minister of the UK?",
@@ -22,14 +64,43 @@ var Quiz = (function(){
 			correctAnswer: 0
 		},
 		{
-			question: "WWhere is the Suez Canal?",
+			question: "Where is the Suez Canal?",
 			choices: ["Cuba", "Australia", "Egypt"],
 			correctAnswer: 2
 		},
 		{
-			question: "WWhat is the chemical symbol for Iron?",
+			question: "What is the chemical symbol for Iron?",
 			choices: ["Fe", "Ir", "Ca"],
 			correctAnswer: 1
 		}
 	];
+
+	function displaySingleQuestion(currentQuestion) {
+
+		var questionTitle = currentQuestion.question;
+
+		_("question-title").appendChild( document.createTextNode(questionTitle) );
+
+		//display choices
+		for (var j = 0; j < currentQuestion.choices.length; j++) {
+
+			var displayed_choices = _("choices").appendChild( document.createElement("input") );
+			displayed_choices.setAttribute("type", "radio");
+			displayed_choices.setAttribute("name", "choices");
+
+			displayed_choices.parentNode.insertBefore(document.createTextNode(currentQuestion.choices[j]), displayed_choices.nextSibling);
+
+			//console.log(choices);
+		}
+	}
+
+	
+
+	//displaySingleQuestion(allQuestions[0]);
+
+
+	
+	window.addEventListener("load", displaySingleQuestion(allQuestions[0]), false);
+	//EventUtil.addHandler(_("submit-button"), "click", displaySingleQuestion(allQuestions));
+
 })();
